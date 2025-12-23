b7a7b0d3ded8614dc2f2673c4d465d5e43df3670
Muhammad Aidil Mirza <144346363+aidlmrza@users.noreply.github.com>
2025-12-23 18:38:23 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,15 +1,11 @@
 import pandas as pd
-import mlflow
-import mlflow.sklearn
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from joblib import dump
 
-# =========================
 # Load dataset
-# =========================
 data = pd.read_csv("diabetes_preprocessing.csv")
 
 X = data.drop(columns=["Outcome"])
@@ -19,15 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-# =========================
-# MLflow config
-# =========================
-mlflow.set_tracking_uri("file:./mlruns")
-mlflow.set_experiment("CI_RandomForest_Diabetes")
-
-# =========================
-# Train model (NO start_run)
-# =========================
+# Train model
 model = RandomForestClassifier(
     n_estimators=100,
     random_state=42
@@ -38,9 +26,8 @@ y_pred = model.predict(X_test)
 
 acc = accuracy_score(y_test, y_pred)
 
-mlflow.log_metric("accuracy", acc)
-
+# Save artifact (optional)
 dump(model, "rf_model.joblib")
-mlflow.log_artifact("rf_model.joblib")
 
-print("Training completed. Accuracy:", acc)
+print("Training completed successfully")
+print("Accuracy:", acc)
